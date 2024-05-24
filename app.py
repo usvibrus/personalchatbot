a33fe573b9b9cf165d62912153ad3d39fab48595
BHAMRE UDAY <97809253+usvibrus@users.noreply.github.com>
2024-05-24 15:06:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -36,8 +36,8 @@ def get_vectorstore(text_chunks):
 
 
 def get_conversation_chain(vectorstore):
-    llm = HuggingFaceHub(repo_id="google/flan-t5-xxl", model_kwargs={"temperature":0.5, "max_length":512})
-
+    llm=HuggingFaceHub(repo_id="google/flan-t5-large", model_kwargs={"temperature":1, "max_length":512})
+    
     memory = ConversationBufferMemory(
         memory_key='chat_history', return_messages=True)
     conversation_chain = ConversationalRetrievalChain.from_llm(
